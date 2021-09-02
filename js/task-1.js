a78e7db9f70dc839def8fd7a01b75eff18b85cec
Dmytro Semeruk <deemoon83@gmail.com>
2021-09-02 15:33:15 +0300

    1

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+
+const keys = Object.keys(user);
+const values = Object.values(user);
+for (let i of keys) {
+    
+}
