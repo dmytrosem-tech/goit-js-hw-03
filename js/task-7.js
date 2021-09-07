c2d71f0ff4da98799d236fa63794a2bb897badf8
Dmytro Semeruk <deemoon83@gmail.com>
2021-09-07 11:25:00 +0300

    fin

--- js/task-7.js
+++ js/task-7.js
@@ -32,7 +32,11 @@ const account = {
    * Вызывает createTransaction для создания объекта транзакции
    * после чего добавляет его в историю транзакций
    */
-  deposit(amount) {},
+  deposit(amount) {
+    this.balance += amount;
+    const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+    this.transactions.push(transaction);
+  },
 
   /*
    * Метод отвечающий за снятие суммы с баланса.
@@ -43,24 +47,54 @@ const account = {
    * Если amount больше чем текущий баланс, выводи сообщение
    * о том, что снятие такой суммы не возможно, недостаточно средств.
    */
-  withdraw(amount) {},
+  withdraw(amount) {
+    this.balance -= amount;
+    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+    this.transactions.push(transaction);
+  },
 
   /*
    * Метод возвращает текущий баланс
    */
-  getBalance() {},
+  getBalance() {
+    return this.balance;
+  },
 
   /*
    * Метод ищет и возвращает объект транзации по id
    */
-  getTransactionDetails(id) {},
+  getTransactionDetails(id) {
+    for (let transaction of this.transactions) {
+      if (transaction.id === id) {
+        return transaction;
+      }
+    }
+    return "Transaction not found";
+  },
 
   /*
    * Метод возвращает количество средств
    * определенного типа транзакции из всей истории транзакций
    */
-  getTransactionTotal(type) {},
+  getTransactionTotal(type) {
+    let total = 0;
+    this.transactions
+      .filter((transaction) => transaction.type === type)
+      .forEach((transaction) => (total += transaction.amount));
+    return total;
+  },
 };
 
-console.log(account);
-console.log(account.createTransaction(30, Transaction.DEPOSIT));
+account.deposit(50);
+account.deposit(130);
+account.withdraw(20);
+account.withdraw(100);
+console.log("Ballance:", account.getBalance());
+console.log("History:", account.transactions);
+console.log("Search with ID:", account.getTransactionDetails(1));
+console.log("Search with ID:", account.getTransactionDetails(5));
+console.log("Total deposit:", account.getTransactionTotal(Transaction.DEPOSIT));
+console.log(
+  "Total withdraw:",
+  account.getTransactionTotal(Transaction.WITHDRAW)
+);
